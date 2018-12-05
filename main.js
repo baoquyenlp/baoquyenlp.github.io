2f12876a061e2ff22545772c702853998eff3460
Long Vu <longvu020898@gmail.com>
2018-12-05 15:49:04 +0700

    okie

---
+++ main.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+	$('.about').animate({
+		opacity: 1},
+		1000);
+	$('.about .avatar').animate({
+		opacity: 1},
+		1500);
+	$('.about .information').animate({
+		opacity: 1},
+		2000);
+});
